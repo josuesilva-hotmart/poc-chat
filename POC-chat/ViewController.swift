a7aaae0991129bbf552b7aa382f5617fb76bea31
Josué Santos <josue.silva@hotmart.com>
2018-10-11 14:25:03 -0300

    Initial Commit

---
+++ POC-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POC-chat
+//
+//  Created by Josué on 11/10/18.
+//  Copyright © 2018 Josué. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
